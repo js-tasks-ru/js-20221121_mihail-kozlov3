37eacb17160a5b184722c29e34a85d15aa71d09a
Morro Luarvik <dr.morro.l@gmail.com>
2022-12-02 17:08:11 +1000

    task 3.1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const ret = function(obj) {
+    let val = obj;
+    for (const key of path.split('.')) {
+      if (!Object.hasOwn(val, key)) {return;}
+      
+      val = val[key];
+    }
 
+    return val;
+  };
+
+  return ret;
 }
