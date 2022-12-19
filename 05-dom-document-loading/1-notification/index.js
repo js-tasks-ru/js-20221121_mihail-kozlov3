9886b260723fe1c5af4575afa3a0c53b857a7151
Morro Luarvik <dr.morro.l@gmail.com>
2022-12-19 10:57:50 +1000

    task 5.1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,63 @@
 export default class NotificationMessage {
+    static current = null;
 
+    constructor(
+      text, 
+      { 
+        duration = 1000,
+        type = "success"
+      } = {}) {
+
+      this.text = text;
+      this.duration = duration;
+      this.type = type;
+      this.timeout = null;
+      this.render();
+    }
+    
+    isVisible() {
+      return this.timeout !== null;
+    }
+    
+    show(target = document.body) {
+      if (NotificationMessage.current) { NotificationMessage.current.remove(); }
+      NotificationMessage.current = this;
+  
+      this.timeout = setTimeout(() => {
+        this.remove();
+      }, this.duration);
+  
+      target.append(this.element);
+    }
+    
+    getTemplate() {
+      return `
+        <div class="notification ${this.type}" style="--value:${this.duration}ms">
+          <div class="timer"></div>
+          <div class="inner-wrapper">
+            <div class="notification-header">${this.type}</div>
+            <div class="notification-body">
+              ${this.text}
+            </div>
+          </div>
+        </div>
+      `;
+    }
+    
+    render() {
+      const element = document.createElement("div");
+      element.innerHTML = this.getTemplate();
+      this.element = element.firstElementChild;
+    }
+    
+    remove() {
+      NotificationMessage.current = null;
+      clearTimeout(this.timeout);
+      this.timeout = null;
+      this.element.remove();
+    }
+    
+    destroy() {
+      this.remove();
+    }    
 }
