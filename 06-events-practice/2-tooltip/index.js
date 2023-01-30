1cde68bb87ff11e77880ae97a7fc1390d4d0514a
Morro Luarvik <dr.morro.l@gmail.com>
2023-01-30 11:41:01 +1000

    task 6.2

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,56 @@
 class Tooltip {
-  initialize () {
+  static activeTooltip;
 
+  pointerOverHandler = (evt) => {
+    if (evt.target.dataset.tooltip) {
+      this.render();
+      this.element.textContent = evt.target.dataset.tooltip;
+      document.addEventListener('pointermove', this.pointerMoveHandler);
+    }
+  }
+
+  pointerMoveHandler = (evt) => {
+    this.element.style.left = evt.clientX + 'px';
+    this.element.style.top = evt.clientY + 'px';
+  }
+
+  pointerOutHandler = (evt) => {
+    if (evt.target.dataset.tooltip) {
+      this.remove();
+      document.removeEventListener('pointermove', this.pointerMoveHandler);
+    }
+  }
+
+  constructor() {
+    if (Tooltip.activeTooltip) { return Tooltip.activeTooltip; }
+
+    this.element = document.createElement('div');
+    this.element.classList.add('tooltip');
+    Tooltip.activeTooltip = this;
+  }
+
+  initialize () {
+    document.addEventListener('pointerover', this.pointerOverHandler);
+    document.addEventListener('pointerout', this.pointerOutHandler);
+  }
+
+  render() {
+    document.body.append(this.element);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    Tooltip.activeTooltip = null;
+
+    document.removeEventListener('pointerover', this.pointerOverHandler);
+    document.removeEventListener('pointermove', this.pointerMoveHandler);
+    document.removeEventListener('pointerout', this.pointerOutHandler);
   }
 }
 
